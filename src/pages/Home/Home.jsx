caba8b743bb3d1ad86e5054f78ba000a8bc58840
myarask <mark@yaraskavitch.com>
2019-09-26 18:59:38 -0400

    Add icons to the services

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ContentSwitch from 'components/ContentSwitch';
+import Activity from 'assets/services/Activity.svg';
+import Cleaning from 'assets/services/Cleaning.svg';
+import Companionship from 'assets/services/Companionship.svg';
+import Cooking from 'assets/services/Cooking.svg';
+import DriverCompanion from 'assets/services/DriverCompanion.svg';
+import Grooming from 'assets/services/Grooming.svg';
+import HelpingAndMobility from 'assets/services/HelpingAndMobility.svg';
+import Hygiene from 'assets/services/Hygiene.svg';
 import Home1 from './Home1';
 import Home2 from './Home2';
 
@@ -26,34 +34,42 @@ class Home extends React.Component {
         {
           id: 'service_1',
           name: 'Driver Companion',
+          src: DriverCompanion,
         },
         {
           id: 'service_2',
           name: 'Hygiene',
+          src: Hygiene,
         },
         {
           id: 'service_3',
           name: 'Helping & Mobility',
+          src: HelpingAndMobility,
         },
         {
           id: 'service_4',
           name: 'Cleaning',
+          src: Cleaning,
         },
         {
           id: 'service_5',
           name: 'Grooming',
+          src: Grooming,
         },
         {
           id: 'service_6',
           name: 'Companionship',
+          src: Companionship,
         },
         {
           id: 'service_7',
           name: 'Cooking',
+          src: Cooking,
         },
         {
           id: 'service_8',
           name: 'Activity',
+          src: Activity,
         },
       ],
     };
--- src/pages/Home/_HomeService.jsx
+++ src/pages/Home/_HomeService.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, ButtonBase, Typography } from '@material-ui/core';
+import { Card, CardContent, ButtonBase, Typography } from '@material-ui/core';
 
 const HomeService = props => (
   <Card square>
-    <ButtonBase onClick={props.onClick} style={{ width: '100%', height: '100px' }}>
-      <CardMedia
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="Contemplative Reptile"
-      />
+    <ButtonBase onClick={props.onClick} style={{ width: '100%', height: '95px' }}>
+      <img src={props.src} alt="" style={{ position: 'absolute', width: '50px', top: '10px' }} />
       <CardContent>
-        <Typography variant="h5" component="h2">
+        <Typography
+          variant="h5"
+          component="h2"
+          style={{ position: 'absolute', left: 0, bottom: '10px', width: '100%' }}
+        >
           {props.name}
         </Typography>
       </CardContent>
