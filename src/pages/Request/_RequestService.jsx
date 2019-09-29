6e5f47b11e4bcdd424d65a721694b44d52c319a4
myarask <mark@yaraskavitch.com>
2019-09-29 12:14:55 -0400

    Add fade in

--- src/pages/Request/_RequestService.jsx
+++ src/pages/Request/_RequestService.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { withTheme } from '@material-ui/styles';
 import { Card, CardContent, ButtonBase, Typography } from '@material-ui/core';
+import Img from 'components/Img';
 
 const RequestService = props => (
   <Card square>
     <ButtonBase onClick={props.onClick} style={{ width: '100%', height: '95px' }}>
-      <img src={props.src} alt="" style={{ position: 'absolute', width: '50px', top: '10px' }} />
-
+      <Img src={props.src} alt={props.name} style={{ position: 'absolute', width: '50px', top: '10px' }} />
       <CardContent>
-        <Typography variant="h5" component="h2" style={{ position: 'absolute', left: 0, bottom: '10px', width: '100%' }}>
+        <Typography
+          variant="h5"
+          component="h2"
+          style={{ position: 'absolute', left: 0, bottom: '10px', width: '100%' }}
+        >
           {props.name}
         </Typography>
       </CardContent>
