61f7fe74aa53cf26a060d4576edac267ae40988a
myarask <mark@yaraskavitch.com>
2019-10-02 23:03:27 -0400

    fix scroll

--- src/pages/Request/Request1Mobile.jsx
+++ src/pages/Request/Request1Mobile.jsx
@@ -4,7 +4,7 @@ import RequestClientSelected from './_RequestClientSelected';
 import RequestService from './_RequestService';
 
 const Request1Mobile = props => (
-  <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', height: '100%', overflow: 'auto' }}>
     <Grid container style={{ flex: 0 }}>
       {props.services.map(service => (
         <Grid item xs={12}>
