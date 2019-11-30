3e0c851c7be4f8e4c640a723151a6efcb05c7337
myarask <mark@yaraskavitch.com>
2019-11-30 10:00:00 -0500

    build

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,25 +4,12 @@ import LoginDesktop from './LoginDesktop';
 import LoginMobile from './LoginMobile';
 import LoginTablet from './LoginTablet';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-
-    this.funcs = {
-      setPageState: this.setState.bind(this),
-    };
-  }
-
-  render() {
-    return (
-      <DeviceSwitch {...this.props} {...this.state} {...this.funcs}>
-        <LoginMobile />
-        <LoginTablet />
-        <LoginDesktop />
-      </DeviceSwitch>
-    );
-  }
-}
+const Login = () => (
+  <DeviceSwitch>
+    <LoginMobile />
+    <LoginTablet />
+    <LoginDesktop />
+  </DeviceSwitch>
+);
 
 export default Login;
