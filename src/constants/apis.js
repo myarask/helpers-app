fb4cd6a9d4c3ae51e0669172fee2a9d8a4080e4a
myarask <mark@yaraskavitch.com>
2019-11-30 10:16:27 -0500

    build

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -1,10 +1,11 @@
 // const host = 'https://helpers-test-api.herokuapp.com';
 const host = 'http://localhost:5000';
 const CLIENTS = `${host}/clients`;
+const HELPERS_ME = `${host}/helpers/me`;
 const SESSIONS = `${host}/sessions`;
 const JOBS = `${host}/jobs`;
 const JOBS_ID = id => `${host}/jobs/${id}`;
 const JOB_SERVICES = `${host}/job_services`;
 const REQUESTERS_ID_CLIENTS = id => `${host}/requesters/${id}/clients`;
 
-export { CLIENTS, SESSIONS, JOBS, JOBS_ID, JOB_SERVICES, REQUESTERS_ID_CLIENTS };
+export { CLIENTS, HELPERS_ME, SESSIONS, JOBS, JOBS_ID, JOB_SERVICES, REQUESTERS_ID_CLIENTS };
--- src/pages/Login/_LoginForm.jsx
+++ src/pages/Login/_LoginForm.jsx
@@ -6,44 +6,45 @@ import { Box, Button } from '@material-ui/core';
 import { TextField } from 'formik-material-ui';
 import { Formik, Form, Field } from 'formik';
 
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  return errors;
+};
+
 const LoginForm = () => {
   const { onLogin } = useContext(AppContext);
-  return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
+
+  const onSubmit = (values, { setSubmitting, setErrors }) => {
+    const options = {
+      auth: {
+        username: values.email,
+        password: values.password,
+      },
+    };
+    return axios
+      .post(SESSIONS, {}, options)
+      .then(({ data }) => onLogin(data))
+      .catch(e => {
         const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-          errors.email = 'Invalid email address';
+
+        if (e.response.status === 401) {
+          errors.password = 'Incorrect password';
+        } else if (e.response.status === 404) {
+          errors.email = 'Incorrect email';
         }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting, setErrors }) => {
-        const options = {
-          auth: {
-            username: values.email,
-            password: values.password,
-          },
-        };
-        return axios
-          .post(SESSIONS, {}, options)
-          .then(({ data }) => onLogin(data))
-          .catch(e => {
-            const errors = {};
 
-            if (e.response.status === 401) {
-              errors.password = 'Incorrect password';
-            } else if (e.response.status === 404) {
-              errors.email = 'Incorrect email';
-            }
+        setErrors(errors);
+        setSubmitting(false);
+      });
+  };
 
-            setErrors(errors);
-            setSubmitting(false);
-          });
-      }}
-    >
+  return (
+    <Formik initialValues={{ email: '', password: '' }} validate={validate} onSubmit={onSubmit}>
       {({ isSubmitting }) => (
         <Form>
           <Field fullWidth type="email" name="email" margin="normal" label="Email" component={TextField} />
