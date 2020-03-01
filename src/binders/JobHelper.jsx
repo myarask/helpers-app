034de081ea65207eb91d1b9af85ad0d25473f9da
myarask <mark@yaraskavitch.com>
2020-02-29 20:38:09 -0500

    lint

--- src/binders/JobHelper.jsx
+++ src/binders/JobHelper.jsx
@@ -24,7 +24,7 @@ const getNextURL = status => {
     case 'complete':
       return links.jobHelperComplete;
     default:
-      return links.jobHelperUnauthorized; // Unauthorized
+      return links.jobHelperUnauthorized;
   }
 };
 
